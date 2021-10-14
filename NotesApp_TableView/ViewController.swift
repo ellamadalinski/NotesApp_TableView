040cec0ed4fc3ce84a8a6970424be20e9a7f4db4
Ella Madalinski <ella.madalinski24@d155.org>
2021-10-14 18:07:57 -0500

    Initial Commit

---
+++ NotesApp_TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesApp_TableView
+//
+//  Created by Ella Madalinski on 10/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
